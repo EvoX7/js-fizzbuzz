8504f73e5b43af5b369d1c686a3537b3b52cb48e
EvoX7 <creativesoultouch@gmail.com>
2022-06-16 16:40:26 +0200

    Starting bonus 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
+
+const myWrapper = document.getElementById ("my_wrapper");
+const FizzBuzz = (numbers % 15 == 0) ;
+const Buzz = (numbers % 5 == 0) ;
+const Fizz = (numbers % 3 == 0) ;
+
+
 // JS Loop exercise 
 
 for (let numbers = 1; numbers < 100 + 1; numbers++) {
+
+    // DIV Creation 
+    const squareBox = document.createElement ("div");
+    squareBox.classList.add ("col-4, my_box");
+
+    // Add content to DIV 
+    squareBox.innerHTML 
+
 
 
     if (numbers % 15 == 0) {
@@ -21,6 +36,8 @@ for (let numbers = 1; numbers < 100 + 1; numbers++) {
     else {
         console.log(numbers);
     }
+
+    
 
 
 } 
