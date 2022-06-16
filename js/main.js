d126262b20504e8f68a0ee60fea9c5f0d7b9f88a
EvoX7 <creativesoultouch@gmail.com>
2022-06-16 17:02:22 +0200

    Modifing bonus 1

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
 
-const myWrapper = document.getElementById ("my_wrapper");
-const FizzBuzz = (numbers % 15 == 0) ;
-const Buzz = (numbers % 5 == 0) ;
-const Fizz = (numbers % 3 == 0) ;
+const myWrapper = document.getElementById("myWrapper");
+
 
 
 // JS Loop exercise 
 
 for (let numbers = 1; numbers < 100 + 1; numbers++) {
 
-    // DIV Creation 
-    const squareBox = document.createElement ("div");
-    squareBox.classList.add ("col-4, my_box");
-
-    // Add content to DIV 
-    squareBox.innerHTML 
-
-
-
     if (numbers % 15 == 0) {
         console.log("FizzBuzz");
-     
+
+
     }
 
     else if (numbers % 5 == 0) {
         console.log("Buzz");
 
+
     }
 
     else if (numbers % 3 == 0) {
         console.log("Fizz");
+
 
     }
 
@@ -37,7 +29,18 @@ for (let numbers = 1; numbers < 100 + 1; numbers++) {
         console.log(numbers);
     }
 
-    
+    let FizzBuzz = (numbers % 15 == 0);
+    let Buzz = (numbers % 5 == 0);
+    let Fizz = (numbers % 3 == 0);
+
+    // DIV Creation 
+    const squareBox = document.createElement("div");
+    squareBox.classList.add("col-1", "myBox");
+
+    // Add content to DIV 
+    squareBox.innerHTML = numbers;
+    myWrapper.append(squareBox);
+
 
 
 } 
