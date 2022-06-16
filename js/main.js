71a107d84ddaf923b6346644cf486cf3fa3886b9
EvoX7 <creativesoultouch@gmail.com>
2022-06-16 15:16:49 +0200

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
 // JS Loop exercise 
 
 for (let numbers = 1; numbers < 100 + 1; numbers++) {
-    
 
-    if( numbers % 3 == 0 ) {
-        console.log("Fizz");
-       
-       
+
+    if (numbers % 15 == 0) {
+        console.log("FizzBuzz");
+     
     }
 
     else if (numbers % 5 == 0) {
         console.log("Buzz");
-       
+
+    }
+
+    else if (numbers % 3 == 0) {
+        console.log("Fizz");
+
+    }
+
+    else {
+        console.log(numbers);
     }
 
 
-    else {
-    console.log(numbers);
-}
-    
-    
 } 
