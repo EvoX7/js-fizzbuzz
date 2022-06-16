08cfcb6c320088fb19954d4f3393c3f02bb5ccd0
EvoX7 <creativesoultouch@gmail.com>
2022-06-16 14:55:28 +0200

    Starting

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// JS Loop exercise 
+
+for (let numbers = 1; numbers < 100 + 1; numbers++) {
+    
+
+    if( numbers % 3 == 0 ) {
+        console.log("Fizz");
+       
+       
+    }
+
+    else if (numbers % 5 == 0) {
+        console.log("Buzz");
+       
+    }
+
+
+    else {
+    console.log(numbers);
+}
+    
+    
+} 
